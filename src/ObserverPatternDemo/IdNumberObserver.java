34e19a740cc8179ba9b187a048fdc51bc4425365
talamajo <35745097+talamajo@users.noreply.github.com>
2018-02-09 21:01:55 -0500

    Update IdNumberObserver.java

--- src/ObserverPatternDemo/IdNumberObserver.java
+++ src/ObserverPatternDemo/IdNumberObserver.java
@@ -1,12 +1,14 @@
 package ObserverPatternDemo;
 
 public class IdNumberObserver implements IObserver {
+	private Student student updater;
 	
 	public IdNumberObserver() {
 		
 	}
 	
-	public void update(Student student) {
+	public void update(Student student updater) {
+		if student.attachObserver(CoursesObserver);
 		System.out.println("Student's id number is " + student.getIdNumber());
 	}
 
