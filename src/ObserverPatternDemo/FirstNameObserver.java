afaf6018c616c39d001cd6840edffc24b03e0d8b
talamajo <35745097+talamajo@users.noreply.github.com>
2018-02-09 21:00:53 -0500

    Update FirstNameObserver.java

--- src/ObserverPatternDemo/FirstNameObserver.java
+++ src/ObserverPatternDemo/FirstNameObserver.java
@@ -1,13 +1,14 @@
 package ObserverPatternDemo;
 
 public class FirstNameObserver implements IObserver {
-	private Student student;
+	private Student student updater;
 	
 	public FirstNameObserver() {
 		
 	}
 	
-	public void update(Student student) {		
+	public void update(Student student updater) {
+		if student.attachObserver(FirstNameObserver);
 		System.out.println("Student's first name is " + student.getFirstName());
 	}
 }
