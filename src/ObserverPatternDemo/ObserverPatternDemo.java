f18784ae49ee269f0a774dc02ae14763dcc5c61c
talamajo <35745097+talamajo@users.noreply.github.com>
2018-02-09 21:02:21 -0500

    Update ObserverPatternDemo.java

--- src/ObserverPatternDemo/ObserverPatternDemo.java
+++ src/ObserverPatternDemo/ObserverPatternDemo.java
@@ -4,14 +4,18 @@ public class ObserverPatternDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Student student = new Student("Garett", "Chang", 123456, "changgar@shu.edu");
+		Student student updater = new Student("Garett", "Chang", 123456, "changgar@shu.edu");
 		
 		IdNumberObserver idObserver =  new IdNumberObserver();
 		FirstNameObserver firstNameObserver = new FirstNameObserver();
 		
-		student.attachObserver(idObserver);
-		student.attachObserver(firstNameObserver);
+		student.attachObserver(FirstNameObserver);
+		student.attachObserver(IdNumberObserver);
+		student.attachObserver(EmailObserver);
+		student.attachObserver(CoursesObserver);
 		
+		student.addCourses("Software Engineering");
+		student.addCourses("Automata")
 		student.setFirstName("Garrett");
 		student.setIdNumber(54321);
 		
